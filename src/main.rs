a6ea07cc3bd4ec6c4ea8ec46f43c367429e2dfab
Ed Jones <ed.jones@canonical.com>
2023-07-30 16:09:37 +0100

    Add auto and overrides

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,18 @@ mod settings;
 
 use std::error::Error;
 
+use chrono::{Datelike, NaiveTime, Utc};
 use clap::Parser;
+use lazy_static::lazy_static;
+use settings::Override;
 
 use crate::args::{Args, Command, Config};
-use crate::settings::Settings;
+use crate::settings::{ProfileType, Settings};
+
+lazy_static! {
+    static ref WORK_START: NaiveTime = NaiveTime::from_hms_opt(6, 0, 0).unwrap();
+    static ref WORK_END: NaiveTime = NaiveTime::from_hms_opt(18, 30, 00).unwrap();
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
@@ -14,7 +22,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("{settings:#?}");
 
     match args.command().unwrap_or(&Default::default()) {
-        Command::Auto => auto(&args),
+        Command::Auto => auto(&args, &settings),
         Command::Work => work(&args),
         Command::Play => play(&args),
         Command::Config(cfg) => config(&args, &cfg, &mut settings),
@@ -24,8 +32,30 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn auto(_args: &Args) -> Result<(), Box<dyn Error>> {
-    todo!("auto");
+fn auto(args: &Args, settings: &Settings) -> Result<(), Box<dyn Error>> {
+    let profile_type = settings
+        .r#override()
+        .map(Override::advise_profile)
+        .flatten()
+        .unwrap_or_else(|| {
+            let now = Utc::now();
+            use chrono::Weekday::*;
+            match now.weekday() {
+                Sat | Sun => ProfileType::Play,
+                _ => {
+                    let time = now.time();
+                    if *WORK_START <= time && time <= *WORK_END {
+                        ProfileType::Work
+                    } else {
+                        ProfileType::Play
+                    }
+                }
+            }
+        });
+    match profile_type {
+        ProfileType::Work => work(args),
+        ProfileType::Play => play(args),
+    }
 }
 
 fn work(_args: &Args) -> Result<(), Box<dyn Error>> {
--- src/settings.rs
+++ src/settings.rs
@@ -5,6 +5,7 @@ use std::{
     path::Path,
 };
 
+use chrono::{offset::Utc, serde::ts_seconds, DateTime, Duration};
 use lazy_static::lazy_static;
 use serde::{Deserialize as Deserialise, Serialize as Serialise};
 
@@ -17,7 +18,10 @@ lazy_static! {
 pub(crate) struct Settings {
     pub(crate) work: Profile,
     pub(crate) play: Profile,
-    pub(crate) override_day: Option<usize>,
+    pub(crate) r#override: Option<Override>,
+
+    #[serde(skip)]
+    dirty: bool,
 }
 
 impl Settings {
@@ -30,6 +34,10 @@ impl Settings {
     }
 
     pub(crate) fn save(&self) -> Result<(), Box<dyn Error>> {
+        if !self.is_dirty() {
+            return Ok(());
+        }
+
         let settings_path = Path::new(&SETTINGS_FILE[..]);
         let settings_dir = settings_path.parent().unwrap();
         fs::create_dir_all(settings_dir)?;
@@ -40,6 +48,35 @@ impl Settings {
             .open(settings_path)?;
         Ok(write!(settings_file, "{}", serde_json::to_string(self)?)?)
     }
+
+    fn is_dirty(&self) -> bool {
+        self.dirty || self.work.dirty || self.play.dirty
+    }
+
+    pub(crate) fn work(&self) -> &Profile {
+        &self.work
+    }
+
+    pub(crate) fn work_mut(&mut self) -> &mut Profile {
+        &mut self.work
+    }
+
+    pub(crate) fn play(&mut self) -> &Profile {
+        &self.play
+    }
+
+    pub(crate) fn play_mut(&mut self) -> &mut Profile {
+        &mut self.play
+    }
+
+    pub(crate) fn r#override(&self) -> Option<&Override> {
+        self.r#override.as_ref()
+    }
+
+    pub(crate) fn declare_override_day(&mut self, profile_type: ProfileType) {
+        let date = Utc::now();
+        self.r#override = Some(Override { date, profile_type })
+    }
 }
 
 impl Default for Settings {
@@ -48,12 +85,14 @@ impl Default for Settings {
             work: Profile {
                 browser: "/usr/bin/firefox".into(),
                 background_dir: "/home/kcza/Pictures/wallpapers/play".into(),
+                dirty: false,
             },
             play: Profile {
                 browser: "/snap/bin/brave".into(),
                 background_dir: "/home/kcza/Pictures/wallpapers/work".into(),
+                dirty: false,
             },
-            override_day: None,
+            r#override: None,
             dirty: false,
         }
     }
@@ -63,4 +102,38 @@ impl Default for Settings {
 pub(crate) struct Profile {
     pub(crate) browser: String,
     pub(crate) background_dir: String,
+
+    #[serde(skip)]
+    dirty: bool,
+}
+
+#[derive(Debug, Serialise, Deserialise)]
+pub(crate) struct Override {
+    profile_type: ProfileType,
+
+    #[serde(with = "ts_seconds")]
+    date: DateTime<Utc>,
+}
+
+impl Override {
+    pub(crate) fn advise_profile(&self) -> Option<ProfileType> {
+        if !self.is_in_force() {
+            return None;
+        }
+        Some(self.profile_type)
+    }
+
+    fn is_in_force(&self) -> bool {
+        self.date < Utc::now() - self.duration()
+    }
+
+    fn duration(&self) -> Duration {
+        Duration::hours(6)
+    }
+}
+
+#[derive(Copy, Clone, Debug, Serialise, Deserialise)]
+pub(crate) enum ProfileType {
+    Work,
+    Play,
 }
