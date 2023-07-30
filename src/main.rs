fdc615cc818f61ed22e74d4e08dda0d82ac6d774
Ed Jones <ed.jones@canonical.com>
2023-07-30 16:27:27 +0100

    Refactor application into `ProfileApplier`

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,14 @@
 mod args;
+mod profile_applier;
 mod settings;
 
 use std::error::Error;
 
-use chrono::{Datelike, NaiveTime, Utc};
 use clap::Parser;
-use lazy_static::lazy_static;
-use settings::Override;
 
 use crate::args::{Args, Command, Config};
+use crate::profile_applier::ProfileApplier;
 use crate::settings::{ProfileType, Settings};
-
-lazy_static! {
-    static ref WORK_START: NaiveTime = NaiveTime::from_hms_opt(6, 0, 0).unwrap();
-    static ref WORK_END: NaiveTime = NaiveTime::from_hms_opt(18, 30, 00).unwrap();
-}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
@@ -22,48 +16,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("{settings:#?}");
 
     match args.command().unwrap_or(&Default::default()) {
-        Command::Auto => auto(&args, &settings),
-        Command::Work => work(&args),
-        Command::Play => play(&args),
+        Command::Auto => ProfileApplier::auto(&args, &settings).apply(),
+        Command::Work => ProfileApplier::new(&args, &settings, ProfileType::Work).apply(),
+        Command::Play => ProfileApplier::new(&args, &settings, ProfileType::Play).apply(),
         Command::Config(cfg) => config(&args, &cfg, &mut settings),
     }?;
 
     settings.save()?;
     Ok(())
-}
-
-fn auto(args: &Args, settings: &Settings) -> Result<(), Box<dyn Error>> {
-    let profile_type = settings
-        .r#override()
-        .map(Override::advise_profile)
-        .flatten()
-        .unwrap_or_else(|| {
-            let now = Utc::now();
-            use chrono::Weekday::*;
-            match now.weekday() {
-                Sat | Sun => ProfileType::Play,
-                _ => {
-                    let time = now.time();
-                    if *WORK_START <= time && time <= *WORK_END {
-                        ProfileType::Work
-                    } else {
-                        ProfileType::Play
-                    }
-                }
-            }
-        });
-    match profile_type {
-        ProfileType::Work => work(args),
-        ProfileType::Play => play(args),
-    }
-}
-
-fn work(_args: &Args) -> Result<(), Box<dyn Error>> {
-    todo!("work");
-}
-
-fn play(_args: &Args) -> Result<(), Box<dyn Error>> {
-    todo!("play");
 }
 
 fn config(_args: &Args, cfg: &Config, settings: &mut Settings) -> Result<(), Box<dyn Error>> {
---
+++ src/profile_applier.rs
@@ -0,0 +1,64 @@
+use std::error::Error;
+
+use chrono::{Datelike, NaiveTime, Utc};
+use lazy_static::lazy_static;
+
+use crate::{
+    args::Args,
+    settings::{Override, Profile, ProfileType, Settings},
+};
+
+lazy_static! {
+    static ref WORK_START: NaiveTime = NaiveTime::from_hms_opt(6, 0, 0).unwrap();
+    static ref WORK_END: NaiveTime = NaiveTime::from_hms_opt(18, 30, 00).unwrap();
+}
+
+pub(crate) struct ProfileApplier<'a> {
+    args: &'a Args,
+    settings: &'a Settings,
+    profile_type: ProfileType,
+}
+
+impl<'a> ProfileApplier<'a> {
+    pub(crate) fn new(args: &'a Args, settings: &'a Settings, profile_type: ProfileType) -> Self {
+        Self {
+            args,
+            settings,
+            profile_type,
+        }
+    }
+
+    pub(crate) fn auto(args: &'a Args, settings: &'a Settings) -> Self {
+        let profile_type = settings
+            .r#override()
+            .map(Override::advise_profile)
+            .flatten()
+            .unwrap_or_else(|| {
+                let now = Utc::now();
+                use chrono::Weekday::*;
+                match now.weekday() {
+                    Sat | Sun => ProfileType::Play,
+                    _ => {
+                        let time = now.time();
+                        if *WORK_START <= time && time <= *WORK_END {
+                            ProfileType::Work
+                        } else {
+                            ProfileType::Play
+                        }
+                    }
+                }
+            });
+        Self::new(args, settings, profile_type)
+    }
+
+    pub(crate) fn apply(&self) -> Result<(), Box<dyn Error>> {
+        match self.profile_type {
+            ProfileType::Work => self.apply_profile(&self.settings.work),
+            ProfileType::Play => self.apply_profile(&self.settings.play),
+        }
+    }
+
+    fn apply_profile(&self, profile: &Profile) -> Result<(), Box<dyn Error>> {
+        todo!("apply profile {:?}", self.profile_type);
+    }
+}
